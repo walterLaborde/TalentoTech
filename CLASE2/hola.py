aa7c502296902da2a28305c1a806ebff29e0735b
walterLaborde <walter.laborde@hotmail.com>
2024-09-16 18:39:27 -0300

    print con variables y f strings

--- CLASE2/hola.py
+++ CLASE2/hola.py
@@ -1,1 +1,12 @@
-print("Hola Mundo!")
+# print con variable
+
+nombre = "Juan"
+
+print(f"Hola {nombre}!")
+
+# print con varias variables y f-strings
+
+a = 2
+b = 3
+
+print(f"La suma de {a} + {b} es : {a+b}")
