1185345ae87cf2b04a58f7fcfdffc2046d271b72
walterLaborde <walter.laborde@hotmail.com>
2024-09-18 09:32:06 -0300

    distintos print

--- CLASE2/hola.py
+++ CLASE2/hola.py
@@ -9,4 +9,19 @@ print(f"Hola {nombre}!")
 a = 2
 b = 3
 
-print(f"La suma de {a} + {b} es : {a+b}")
+print(f"La suma de {a} + {b} es igual a {a+b}")
+
+# función print end
+
+print("primer print", end=" ")
+print("segundo print en el mismo renglon")
+
+
+# función print \n
+
+print("escribimos\nen\n4\nlineas")
+
+# funcion print \t (tabulador)
+
+print("nombre\tapellido\tedad")
+print("juancho\tronga\t28x7")
