e3c8772bc5a439142e746f5227d76c45dac6010b
walterLaborde <walter.laborde@hotmail.com>
2024-09-18 09:36:02 -0300

    print para separar

--- CLASE2/hola.py
+++ CLASE2/hola.py
@@ -3,25 +3,25 @@
 nombre = "Juan"
 
 print(f"Hola {nombre}!")
-
+print()
 # print con varias variables y f-strings
 
 a = 2
 b = 3
 
 print(f"La suma de {a} + {b} es igual a {a+b}")
-
+print()
 # función print end
 
 print("primer print", end=" ")
 print("segundo print en el mismo renglon")
-
+print()
 
 # función print \n
 
 print("escribimos\nen\n4\nlineas")
-
+print()
 # funcion print \t (tabulador)
 
 print("nombre\tapellido\tedad")
-print("juancho\tronga\t28x7")
+print("juancho\tronga\tt28x7")
