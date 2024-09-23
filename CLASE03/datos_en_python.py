3dc7b53f953bde019336c9184d5d15f4b77c494d
walterLaborde <walter.laborde@hotmail.com>
2024-09-23 12:42:46 -0300

    conversion de tipo de datos

---
+++ CLASE03/datos_en_python.py
@@ -0,0 +1,18 @@
+### CONVERSION DE TIPO DE DATOS ###
+
+"""
+con las funciones int, float, str podemos convertir los tipos para manipularlos como sea necesario
+"""
+
+cadena = "1234"
+entero = 45
+flotante = 56.2
+
+print(int(cadena)) # convierte a entero (1234)
+print()
+print(float(cadena)) # convierte a coma flotante (1234.0)
+print()
+print(str(entero)) # convierte a cadena ("45")
+print()
+print(str(flotante)) # convierte a cadena ("56.2")
+
