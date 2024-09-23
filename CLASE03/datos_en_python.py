6c6987519fb4f92a2e945ad17b0a21c5eb8e2682
walterLaborde <walter.laborde@hotmail.com>
2024-09-23 15:36:57 -0300

    Función input

--- CLASE03/datos_en_python.py
+++ CLASE03/datos_en_python.py
@@ -39,3 +39,17 @@ print()
     Una expresión es una unidad de código que devuelve un valor y está formada por operandos y operadores
 - Operadores aritméticos : +, -, *, /, %, //, ** (potencia)    
 """
+
+### FUNCIÓN INPUT ###
+
+"""
+Es una forma de ingresar información en el programa.
+Lee lo que se escribe y lo ingresa como STRING. 
+    - Si necesitamos utilizar el valor ingresado con otro formato (nro, bool, etc) hay que convertirlo.
+"""
+
+num1 = input("Ingrese un número: ")
+numero = float(num1)
+resultado = numero * 2
+print(numero,"x 2 =", resultado)
+
