197f48ee3274da24022fd5f6800b52c75956ab3a
walterLaborde <walter.laborde@hotmail.com>
2024-09-23 16:02:58 -0300

    ejercicio 2

--- CLASE03/datos_en_python.py
+++ CLASE03/datos_en_python.py
@@ -111,3 +111,37 @@ print(f"La multiplicación de tus números es {multiplicacion}")
 print()
 print(f"El resto de la división de tus números es {modulo}")
 print()
+
+
+# ejercicio 2
+# calculadora de propinas
+
+"""
+Escribe un programa en Python que calcule la propina que se debe dejar en un restaurante.
+
+El script debe solicitar al usuario el monto total de la cuenta y el porcentaje de propina que desea
+dejar.
+
+Utilizando operadores aritméticos, calcula la cantidad de propina y el total a pagar (incluyendo
+la propina).
+
+Finalmente, muestra los resultados en la pantalla.
+"""
+
+print()
+print()
+print()
+
+# ingreso
+monto_total = float(input("Ingrese el monto total de la cuenta: "))
+porcentaje_propina = float(input("Ingrese el porcentaje que desea dejar: "))
+
+# proceso
+
+propina = monto_total * (porcentaje_propina / 100)
+
+# salida
+
+print()
+print()
+print(f"El total a pagar (incluyendo la propina) es de ${monto_total + propina}")
