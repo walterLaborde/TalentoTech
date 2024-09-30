a67a820942a1db2eff939a2a0a44811d9270e052
walterLaborde <walter.laborde@hotmail.com>
2024-09-30 17:42:40 -0300

    sintaxis condicionales

---
+++ CLASE05/condicionales.py
@@ -0,0 +1,8 @@
+# así se escribe un condicional
+
+edad_ingresada = int(input("Ingrese su edad: "))
+
+if edad_ingresada >= 18:
+    print("Usted es mayor de edad")
+else: 
+    print("Usted es menor de edad")
