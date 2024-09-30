92a4fd1c427cf4588d3fb7125632b3f1b60352db
walterLaborde <walter.laborde@hotmail.com>
2024-09-30 19:14:51 -0300

    modif ejercicio en clase

--- CLASE04/ejercicio_en_clase.py
+++ CLASE04/ejercicio_en_clase.py
@@ -3,15 +3,21 @@
 #por pantalla, la cantidad de alumnos total, y el porcentaje de cada genero.
 
 # solicito ingreso de datos
+print()
 mujeres = int(input("Ingrese la cantidad de mujeres en el curso: "))
+print()
 varones = int(input("Ingrese la cantidad de varones en el curso: "))
+print()
 
 # genero los resultados y los asigno a sus variables
 total_alumnos = mujeres + varones
-porcentaje_mujeres = (mujeres / total_alumnos) * 100 
-porcentaje_varones = (varones / total_alumnos) * 100 
+porcentaje_mujeres = (mujeres / total_alumnos) * 100
+porcentaje_varones = (varones / total_alumnos) * 100
 
 # imprimo los resultados
-print(f"La cantidad total de alumnos es de {total_alumnos}")
-print(f"El porcentaje de mujeres es {porcentaje_mujeres} %")
-print(f"El porcentaje de varones es {porcentaje_varones} %")
+print()
+print(f"La cantidad total de alumnos en el curso es de {total_alumnos}")
+print()
+print(f"El porcentaje de mujeres es {porcentaje_mujeres:.2f} %")
+print()
+print(f"El porcentaje de varones es {porcentaje_varones:.2f} %")
