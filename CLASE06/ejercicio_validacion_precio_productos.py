7398a04d8b5478c0c3b5f8d450b2e286c5c4ee76
walterLaborde <walter.laborde@hotmail.com>
2024-10-09 21:06:54 -0300

    validar precio de producto

---
+++ CLASE06/ejercicio_validacion_precio_productos.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+# Agrega la ruta de la carpeta raíz al PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+#importa el modulo utils para funciones comunes a todas las carpetas
+from utils.utils import formato_moneda
+
+"""
+Escribí un programa que permita ingresar el precio de un producto, pero que sólo acepte
+valores mayores a 0. Si la persona ingresa un valor inválido (negativo o cero), el programa
+debe mostrar un mensaje de error y solicitar nuevamente el valor hasta que se ingrese uno
+válido. Al final, el programa debe mostrar el precio aceptado.
+Tips:
+● Antes de empezar, pensá si es necesario usar contadores o acumuladores.
+● Recordá que input() siempre devuelve cadenas de caracteres.
+"""
+
+validar_precio_producto = int(input("Ingrese el precio del producto: "))
+
+
+while validar_precio_producto <=0:
+    print("El precio ingresado no es válido. Debe ser mayor a cero")
+    validar_precio_producto = int(input("Ingrese nuevamenteel precio del producto: "))
+
+print(f"El precio ingresado es {formato_moneda(validar_precio_producto)}")
