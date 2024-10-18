85ae70d07422095044d0d988418f0f8a46f1073c
walterLaborde <walter.laborde@hotmail.com>
2024-10-17 23:09:07 -0300

    metodos de listas

---
+++ CLASE07/listas.py
@@ -0,0 +1,16 @@
+# declarar una lista
+
+mi_lista = list()
+
+mi_lista.append("hola")
+mi_lista.append("chau")
+print(mi_lista)
+mi_lista.insert(1,"quizas")
+print(mi_lista)
+
+mi_lista.remove("hola")
+mi_lista.append(", buenas tardes")
+print(mi_lista)
+mi_lista.reverse()
+print(mi_lista)
+
