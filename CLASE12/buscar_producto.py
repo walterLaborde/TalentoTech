524da07a80972466b8a8774ec626e6dd0e68670d
walterLaborde <walter.laborde@hotmail.com>
2024-12-13 17:41:51 -0300

    clase12 buscar_producto

---
+++ CLASE12/buscar_producto.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+# Agrega la ruta de la carpeta raíz al PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from utils.colorama_config import CYAN,YELLOW,MAGENTA,LIGHT_RED,RED, GREEN, RESET
+
+from mostrar_productos import inventario_prueba, imprimir_inventario_actual
+from registrar_producto import ingreso_de_elemento
+
+"""
+Frecuentemente necesitamos conocer datos de un único producto. 
+Si bien la función mostrar_productos() que escribiste antes hace esto, 
+lo cierto es que si tenemos muchos productos el listado puede ser demasiado extenso.
+
+Podés crear una función más especializada (a la que llamamos buscar_producto() ) 
+que  le pida a la persona que opera el programa ingresar el código del producto que está buscando, 
+y si el producto existe en el inventario, mostrar la información de ese único producto, 
+con un formato claro. 
+
+TIP: Si el código que se ingresa no está registrado, podemos avisar que no se encontró el producto.
+
+"""
+
+def buscar_producto():
+    codigo = input("Ingrese el código del producto buscado: ")
+    if codigo in inventario_prueba:
+        inventario_prueba.get(codigo)
+        imprimir_inventario_actual({codigo : inventario_prueba[codigo]})
+    else:
+        print(f"{RED}No existe un producto asociado al código {codigo}.")
+
+def main():
+    buscar_producto()
+
+if __name__ == '__main__':
+    main()
+
